b273169018431d94d76bd74a4b3584495c435d30
Mubarek <97316195+mubacode@users.noreply.github.com>
2025-05-04 20:14:56 +0200

    Add files via upload

---
+++ Generic-4/Generic/GenericMethods.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class GenericMethods
+{
+    // 1. Method to swap values of two parameters of the same type
+    public static void Swap<T>(ref T first, ref T second)
+    {
+        T temp = first;
+        first = second;
+        second = temp;
+    }
+
+    // 2. Method accepting two parameters of different types
+    public static void DisplayAndReset<T1, T2>(T1 param1, T2 param2)
+    {
+        Console.WriteLine($"Type of param1: {typeof(T1).Name}");
+        Console.WriteLine($"Type of param2: {typeof(T2).Name}");
+        Console.WriteLine($"Value of param1: {param1}");
+        Console.WriteLine($"Value of param2: {param2}");
+
+        param1 = default(T1);
+        param2 = default(T2);
+    }
+
+    // 3. Method returning new object of specified type
+    public static T CreateNew<T>() where T : new()
+    {
+        return new T();
+    }
+
+    // 4. Method returning larger of two parameters
+    public static T GetLarger<T>(T first, T second) where T : IComparable<T>
+    {
+        return first.CompareTo(second) > 0 ? first : second;
+    }
+
+    // 5. Method returning sorted list of parameters
+    public static List<T> GetSortedList<T>(params T[] items) where T : IComparable<T>
+    {
+        List<T> list = new List<T>(items);
+        list.Sort();
+        return list;
+    }
+
+    // 6. Method creating dictionary with single entry
+    public static Dictionary<TKey, TValue> CreateDictionary<TKey, TValue>(TKey key, TValue value)
+        where TKey : struct
+        where TValue : class
+    {
+        Dictionary<TKey, TValue> dict = new Dictionary<TKey, TValue>();
+        dict.Add(key, value);
+        return dict;
+    }
+
+    // 7. Method to display dictionary elements
+    public static void DisplayDictionary<TKey, TValue>(Dictionary<TKey, TValue> dict)
+    {
+        foreach (var kvp in dict)
+        {
+            Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+        }
+    }
+
+    // 8. Method returning queue or stack based on parameter count
+    public static IEnumerable<T> CreateCollection<T>(params T[] items)
+    {
+        if (items.Length < 3)
+        {
+            Queue<T> queue = new Queue<T>(items);
+            return queue;
+        }
+        else
+        {
+            Stack<T> stack = new Stack<T>(items);
+            return stack;
+        }
+    }
+}
---
+++ Generic-4/Generic/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Test generic methods
+        Console.WriteLine("Testing Generic Methods:");
+
+        // Test Swap
+        int a = 5, b = 10;
+        Console.WriteLine($"Before swap: a={a}, b={b}");
+        GenericMethods.Swap(ref a, ref b);
+        Console.WriteLine($"After swap: a={a}, b={b}");
+
+        // Test Person and Car classes
+        Console.WriteLine("\nTesting Person and Car classes:");
+        var person1 = new Person("John", "Doe", 30, new List<Car>
+        {
+            new Car("Toyota", 25000),
+            new Car("Honda", 22000)
+        });
+
+        var person2 = new Person("Jane", "Smith", 25, new List<Car>
+        {
+            new Car("BMW", 50000)
+        });
+
+        // Test IEnumerable implementation
+        Console.WriteLine("\nJohn's cars:");
+        foreach (var car in person1)
+        {
+            Console.WriteLine(car);
+        }
+
+        // Test sorting people by total car value
+        var people = new List<Person> { person1, person2 };
+        people.Sort();
+        Console.WriteLine("\nPeople sorted by total car value:");
+        foreach (var person in people)
+        {
+            Console.WriteLine(person);
+        }
+
+        // Test Quartet
+        Console.WriteLine("\nTesting Quartet:");
+        var quartets = new List<Quartet<int, string, double, bool>>
+        {
+            new Quartet<int, string, double, bool>(2, "Second", 2.5, true),
+            new Quartet<int, string, double, bool>(1, "First", 1.5, false),
+            new Quartet<int, string, double, bool>(3, "Third", 3.5, true)
+        };
+
+        quartets.Sort();
+        Console.WriteLine("Sorted Quartets:");
+        foreach (var quartet in quartets)
+        {
+            Console.WriteLine(quartet);
+        }
+    }
+}
